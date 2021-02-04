2d0242641ed16a1ecff17cae61403d5f8c8beb57
giovannabloise <75636469+giovannabloise@users.noreply.github.com>
2021-02-04 17:31:22 -0300

    Create pipeline.sh

---
+++ code/pipeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export OUT=../output/
+
+#alinhamento
+
+cd ../data
+
+bwa mem -M -t 40 hg19.fasta 510-7-BRCA_S8_L001_R1_001.fastq.gz 510-7-BRCA_S8_L001_R2_001.fastq.gz > ${OUT}BRCA_aligned.sam
+
+
+#transformar .sam em .bam, gerar .bai, sortir
+
+samtools sort ${OUT}BRCA_aligned.sam > ${OUT}BRCA_aligned.bam
+
+samtools sort ${OUT}BRCA_aligned.sam > ${OUT}BRCA_aligned.sorted.bam
+
+samtools index ${OUT}BRCA_aligned.sorted.bam
+
+
+#realizar a genotipagem
+
+freebayes -f hg19.fasta ${OUT}BRCA_aligned.sorted.bam -v ${OUT}BRCA_var.vcf --targets BRCA.list
+
+#Realizar a anotação
+
+snpEff ann hg19 ${OUT}BRCA_var.vcf > ${OUT}BRCA_var.ann.vcf
